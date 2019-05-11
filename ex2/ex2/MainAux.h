3418ddbfaeb46819daf729f116b11be871f0ea7b
Dan <dantav@gmail.com>
2019-05-11 16:28:48 +0300

    ddd

--- ex2/ex2/MainAux.h
+++ ex2/ex2/MainAux.h
@@ -99,7 +99,7 @@ bool fileExists(const char* path);
 char getMoveChar(const Move& a);
 void updateCoordinate(Coordinate & c, const int i, const int j);
 bool endsWith(const string & mainStr, const string & toMatch);
-bool initPaths(int argc, char * argv[], string (&paths)[3], bool (&pathExists)[3]);
-void parsePairOfArguments(char * type, char * path, bool & validArgs, string (&paths)[3], bool (&pathExists)[3]);
+bool initPaths(int argc, char * argv[], vector<string>& paths, vector<bool>& pathExists);
+void parsePairOfArguments(char * type, char * path, bool & validArgs, vector<string>& paths, vector<bool>& pathExists);
 
 #endif
