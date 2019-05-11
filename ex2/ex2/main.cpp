23e2bfaf59e129fa3e54e331788455750b3047e8
Dan <dantav@gmail.com>
2019-05-11 16:27:10 +0300

    dd

--- ex2/ex2/main.cpp
+++ ex2/ex2/main.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc, char* argv[]) {
 	// MAZEPATH_INDEX = 0, ALGOPATH_INDEX = 1, OUTPUTPATH_INDEX = 2
-	string paths[3] = { ".", ".", "" };
-	bool pathExists[3];
+	vector<string> paths = { ".", ".", "" };
+	vector<bool> pathExists = { false, false, false };
 	bool validArgs = initPaths(argc, argv, paths, pathExists);
 	if (!validArgs) {
 		printWrongArgumentsFormatError();
